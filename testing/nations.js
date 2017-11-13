1de59faa177eb121f2a8fe47046af5a42eba70f6
Gunn Malene Olsen <gunnmaleneolsen@Gunns-MacBook-Pro.local>
2017-11-13 09:15:25 +0100

    nyuu

---
+++ testing/nations.js
@@ -0,0 +1,19 @@
+
+
+class Nation {
+    constructor(navn,leder,tittel,hovedstad) {
+        this.navn = navn;
+        this.leder = leder;
+        this.tittel = tittel;
+        this.hovedstad = hovedstad;
+
+    }
+
+}
+
+class Player {
+    constructor(navn,nation,) {
+        this.navn = navn;
+        this.nation = nation;
+    }
+}
